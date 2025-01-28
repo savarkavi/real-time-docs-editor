5ef325b78f6c8463bfa562778d61c31492f8e03b
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-01-28 15:13:54 +0530

    Enhance editor functionality with additional Tiptap extensions and custom styles

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,19 +1,56 @@
 "use client";
 
+import "./styles.css";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import Image from "@tiptap/extension-image";
+import Underline from "@tiptap/extension-underline";
+import ResizeImage from "tiptap-extension-resize-image";
+import { useEditorStore } from "@/store/use-editor";
 
 const Editor = () => {
+  const { setEditor } = useEditorStore();
+
   const editor = useEditor({
+    onCreate: ({ editor }) => setEditor(editor),
+    onDestroy: () => setEditor(null),
+    onUpdate: ({ editor }) => setEditor(editor),
+    onSelectionUpdate: ({ editor }) => setEditor(editor),
+    onTransaction: ({ editor }) => setEditor(editor),
+    onFocus: ({ editor }) => setEditor(editor),
+    onBlur: ({ editor }) => setEditor(editor),
+    onContentError: ({ editor }) => setEditor(editor),
+    immediatelyRender: false,
     editorProps: {
       attributes: {
         style: "padding-left: 56px; padding-right:56px;",
         class:
-          "focus:outline-none bg-white border border-[#c7c7c7] w-[800px] h-full bg-white py-10",
+          "focus:outline-none bg-stone-950 text-amber-100 w-[800px] h-full bg-white py-10",
       },
     },
-    extensions: [StarterKit],
-    content: "<p>Hello World! 🌎️</p>",
+    extensions: [
+      StarterKit,
+      TaskItem.configure({ nested: true }),
+      TaskList,
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
+      Image,
+      ResizeImage,
+      Underline,
+    ],
+    content: `
+  Hello there
+  `,
   });
 
   return (
