547cd575d53a523f2bfeeed7ab7e3847a86cf897
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-02-07 20:20:46 +0530

    Update TemplateGallery component styling for improved visual hierarchy and interactivity

--- src/app/(home)/templateGallery.tsx
+++ src/app/(home)/templateGallery.tsx
@@ -6,8 +6,8 @@ const TemplateGallery = () => {
   return (
     <div className="w-full px-4 py-8 font-serif">
       <div className="mx-auto flex w-full max-w-[1000px] flex-col gap-6">
-        <h2 className="font-3xl text-amber-100">Start a new Document</h2>
-        <div className="flex h-[250px] w-[200px] cursor-pointer items-center justify-center rounded-lg border border-muted-foreground bg-stone-950 p-4">
+        <h2 className="text-2xl text-amber-100">Start a new Document</h2>
+        <div className="flex h-[250px] w-[200px] cursor-pointer items-center justify-center rounded-lg border border-muted-foreground bg-stone-950 p-4 hover:border-amber-500">
           <PlusIcon className="size-16 text-amber-500" />
         </div>
       </div>
