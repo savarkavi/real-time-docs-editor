b9f2a9707d5b07de0724bc97f8e31a8f1931a4bd
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-02-10 15:34:24 +0530

    Add search functionality to documents query in Home component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,16 +5,22 @@ import DocumentsTable from "./documentsTable";
 import Navbar from "./navbar";
 import TemplateGallery from "./templateGallery";
 import { api } from "../../../convex/_generated/api";
+import { parseAsString, useQueryState } from "nuqs";
 
 export default function Home() {
+  const [search] = useQueryState(
+    "search",
+    parseAsString.withDefault("").withOptions({ clearOnDefault: true }),
+  );
+
   const { results, loadMore, status, isLoading } = usePaginatedQuery(
     api.documents.get,
-    {},
+    { search },
     { initialNumItems: 5 },
   );
 
   return (
-    <div className="min-h-screen bg-black py-10">
+    <div className="min-h-screen bg-black pb-10">
       <Navbar />
       <TemplateGallery />
       <DocumentsTable
