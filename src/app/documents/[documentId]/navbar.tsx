67a9a13423e297a7a4bfca0770b4836fea15879b
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-02-18 19:22:35 +0530

    Add organization switcher and user button to Navbar component

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -3,10 +3,12 @@ import Link from "next/link";
 import { BsCloudCheck } from "react-icons/bs";
 
 import NavbarMenu from "./navbarMenu";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import Inbox from "./inbox";
 
 const Navbar = () => {
   return (
-    <div className="p-4">
+    <div className="flex items-center justify-between p-4">
       <div className="flex items-center gap-4">
         <Link href="/">
           <div className="relative h-12 w-16">
@@ -19,6 +21,28 @@ const Navbar = () => {
             <BsCloudCheck />
           </div>
           <NavbarMenu />
+        </div>
+      </div>
+      <div className="flex items-center gap-2">
+        <Inbox />
+        <div className="flex items-center gap-2">
+          <OrganizationSwitcher
+            appearance={{
+              elements: {
+                organizationSwitcherTriggerIcon: "text-muted-foreground",
+                organizationPreviewMainIdentifier: "text-muted-foreground",
+                organizationPreviewAvatarBox: "w-8 h-8",
+                userPreviewAvatarBox: "w-8 h-8",
+              },
+            }}
+          />
+          <UserButton
+            appearance={{
+              elements: {
+                userButtonAvatarBox: "w-10 h-10",
+              },
+            }}
+          />
         </div>
       </div>
     </div>
