5dd0929cbd39ae4bc24c8b93c1ffcaf0c2d2b52b
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-02-18 19:23:50 +0530

    Increase z-index of Marker component and update border color for better visibility

--- src/app/documents/[documentId]/marker.tsx
+++ src/app/documents/[documentId]/marker.tsx
@@ -10,7 +10,7 @@ interface MarkerProps {
 const Marker = ({ position, isLeft, onMouseDown, isDragging }: MarkerProps) => {
   return (
     <div
-      className="absolute z-[10] cursor-ew-resize"
+      className="absolute z-[999] cursor-ew-resize"
       style={{
         [isLeft ? "left" : "right"]: `${position}px`,
         translate: isLeft ? "-50%" : "50%",
@@ -19,7 +19,7 @@ const Marker = ({ position, isLeft, onMouseDown, isDragging }: MarkerProps) => {
     >
       <ChevronDown className="size-8 text-amber-200" />
       <div
-        className={`absolute left-1/2 top-5 h-screen w-[1px] -translate-x-1/2 border border-dotted ${isDragging ? "block" : "hidden"}`}
+        className={`absolute left-1/2 top-5 h-screen w-[1px] -translate-x-1/2 border border-dotted border-amber-100 ${isDragging ? "block" : "hidden"}`}
       />
     </div>
   );
