a270e4a4ace9eb56f5c42f50e1dce713662c8f47
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-02-22 10:03:22 +0530

    Fetch document by ID and redirect if not found; pass document to Navbar

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,20 +1,31 @@
-import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { api } from "../../../../convex/_generated/api";
+import { Id } from "../../../../convex/_generated/dataModel";
 import Editor from "./editor";
 import Navbar from "./navbar";
 import { Room } from "./room";
 import Toolbar from "./toolbar";
+import { fetchQuery } from "convex/nextjs";
 
 interface DocumentIdPageParams {
-  params: Promise<{ documentId: string }>;
+  params: Promise<{ documentId: Id<"documents"> }>;
 }
 
 const DocumentIdPage = async ({ params }: DocumentIdPageParams) => {
   const { documentId } = await params;
 
+  const document = await fetchQuery(api.documents.getDocumentById, {
+    id: documentId,
+  });
+
+  if (!document) {
+    redirect("/");
+  }
+
   return (
     <Room>
       <div className="min-h-screen w-full overflow-x-hidden bg-stone-950 text-black">
-        <Navbar />
+        <Navbar document={document} />
         <Toolbar />
         <Editor />
       </div>
